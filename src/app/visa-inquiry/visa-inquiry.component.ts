c1d238e63b85fc648100b90198c892b088dfb8e2
AkshitaKaretiya <karetiyaakshita@gmail.com>
2019-12-05 19:17:00 +0530

    inquiry form done

--- src/app/visa-inquiry/visa-inquiry.component.ts
+++ src/app/visa-inquiry/visa-inquiry.component.ts
@@ -8,12 +8,14 @@ import { Router } from '@angular/router';
 export class VisaInquiryComponent implements OnInit {
   formUrl: any = [];
   constructor(public route: Router) {
+
+  }
+
+  ngOnInit() {
     this.formUrl = JSON.parse(localStorage.getItem('formId'));
     this.formUrl.splice(0, 1);
     localStorage.setItem('formId', JSON.stringify(this.formUrl));
   }
-
-  ngOnInit() { }
 
   nextForm() {
     this.route.navigate(['/home/' + this.formUrl[0]])
